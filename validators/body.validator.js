9be113b7c6b2f8083bb47eff5fdb6f347b8c9b4a
Dean Itzhak <Deanitzhak@gmail.com>
2024-03-07 01:36:22 +0200

    Delete validators directory

--- validators/body.validator.js
+++
@@ -1,6 +0,0 @@
-const { BodyNotSent } = require("./../errors/BadRequest.errors");
-exports.bodyValidator = (req) => {
-  if (req.body.constructor === Object && Object.keys(req.body).length === 0)
-    throw new BodyNotSent();
-  return true;
-};
