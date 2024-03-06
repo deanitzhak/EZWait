a7e11d5f888b1ee070415cc04eae1d7f1e3dfa8e
shir149 <shiramar0401@gmail.com>
2024-03-06 21:23:59 +0200

    profileWorking

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*envirment config*/
+/envirment config/
 require('dotenv').config({ path: './.env' });
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,16 +8,20 @@ const nodemailer = require('nodemailer');
 const rateLimit = require("express-rate-limit");
 
 
-/*APIs*/
+/APIs/
+const profileRouter = require('./routers/profileRouter');
+
 const fileLoaderRouter = require('./routers/fileLoaderRouter');
 const loginRouter = require('./routers/logInRouter');
 const appointmentRouter = require('./routers/appointmentRouter');
 const userRouter = require('./routers/userRouter');
+// const messegeReplayRouter = require('./routers/messegeReplayRouter');
+// const messegeSentRouter = require('./routers/messegeSentRouter');
 const scheduleRouter = require('./routers/scheduleRouter');
 const schedulerRouter = require('./routers/schedulerRouter');
 const clientRouter = require('./routers/clientRouter');
 const MongoStorage = require('./db/mongo.storage');
-/*inintialize environment exucting*/
+/inintialize environment exucting/
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT ;
@@ -26,14 +30,17 @@ app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static(path.join(__dirname, 'frontend'))); 
-/*inintialize Routers*/
+/inintialize Routers/
 app.use('/login', loginRouter);
 app.use('/appointment', appointmentRouter);
 app.use('/client', clientRouter);
+app.use('/profile',profileRouter);
 app.use('/user', userRouter);
+// app.use("/messegeReplay",messegeReplayRouter);
+// app.use("/messegeSent",messegeSentRouter);
 app.use("/schedule",scheduleRouter);
 app.use('/scheduler', schedulerRouter);
-/*Mongo connectig*/
+/Mongo connectig/
 const mongoStorageInstance = new MongoStorage();
 mongoStorageInstance.connect()
     .then(() => {
@@ -42,7 +49,7 @@ mongoStorageInstance.connect()
     .catch((err) => {
         console.error("Failed to connect to MongoDB:", err);
     });
-/*server*/
+/server/
 const server = app.listen(port, () => {
     console.log("Server listening on port:", port);
 });
@@ -173,4 +180,3 @@ const sendEmail = (parsedObj, sendStatus) => {
     });
 
 }
-
