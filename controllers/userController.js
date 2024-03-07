0e7643e30bf2b8cd6f23520707ff87c9123f2192
shir149 <80071458+shir149@users.noreply.github.com>
2024-03-07 09:10:30 +0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const globalData = require('../models/myUser.singleton');
 module.exports = {
     getUserData: (req, res) => {
         const myUser = globalData.getData("myUser");
+          res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/json");
         res.send(myUser);
     },
 };
