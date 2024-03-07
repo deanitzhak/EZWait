b1bfe53e4928345940acd9eca1695f306f73a63d
shir149 <80071458+shir149@users.noreply.github.com>
2024-03-07 08:52:57 +0200

    Update logInControler.js

--- controllers/logInControler.js
+++ controllers/logInControler.js
@@ -1,117 +1,20 @@
-const client = require('../models/client.model');
-const clientRepository = require('../repository/client.repository');
-const clientService = require('../service/clientService');
-const appRepo = new clientRepository(client);
-const Path = require('path'); // Ensure you have this required module if not already
-
+const ProfileRepository = require('../repository/profile.repository');
+const User = require('../models/profile.model');
+const globalData = require('../models/myUser.singleton');
 module.exports = {
-    getAllclient: (req, res) => {
-        appRepo.find()
-            .then(clients => {
-                res.send(clients);
-            })
-            .catch(err => {
-                console.error("Error retrieving client:", err);
-                res.status(500).send("Internal server error");
-            });
-    },
-    findAllByUserName: (req, res) => {
-        appRepo.findByUserName(req.body.userName)
-            .then(clients => {
-                res.send(clients);
-            })
-            .catch(err => {
-                console.error("Error retrieving client:", err);
-                res.status(500).send("Internal server error");
-            });
-    },
-    findClientByAppId: (req, res) => {
-        appRepo.findClientByAppId(req.body._id)
-            .then(client => {
-                if (client) {
-                    res.send(client);
-                } else {
-                    res.status(404).send("Client not found");
-                }
-            })
-            .catch(err => {
-                console.error("Error retrieving client:", err);
-                res.status(500).send("Internal server error");
-            });
-    },
-    async findClientByIdAndDelete(req, res) {
-        try {
-            await appRepo.findByIdAndDelete(req.query._id);
-            res.status(200).send("Deleted");
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("Internal server error");
-        }
-    },
-    async findAllClientByStatus(req, res) {
-        try {
-            const clients = await appRepo.findByStatus(req.query.status);
-            res.status(200).send(clients);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("Internal server error");
-        }
-    },
-    async submitNewClient(req, res) {
-        try {
-            console.log("hey");
-            const newApp = await clientService.createNewClient(req.body);
-            console.log("this is my clint : - >", newApp);
-            appRepo.create(newApp);
-            res.status(200).send("New client created successfully");
-
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("Internal server error");
-        }
-    },
-    findClientByAppIdAndUpdateStatus: (req, res) => {
-        const clientId = req.body._id;
-        const newStatus = "block"; // Define the new status, for example, "cancelled"
-
-        // Update the appointment status in the database
-        appRepo.updateClientStatus(clientId, newStatus)
-            .then(updatedClient => {
-                if (updatedClient) {
-                    res.send(updatedClient);
-                } else {
-                    res.status(404).send("client not found");
-                }
-            })
-            .catch(err => {
-                console.error("Error updating client status:", err);
-                res.status(500).send("Internal server error");
-            });
-    },
-    // Adding the new code here
-    serveFile: (req, res) => {
-        res.status(200);
-        try {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-
-            switch (Path.extname(pathName)) {
-                case ".js":
-                    res.setHeader("Content-Type", "text/javascript");
-                    break;
-                case ".css":
-                    res.setHeader("Content-Type", "text/css");
-                    break;
-                case ".html":
-                    res.setHeader("Content-Type", "text/html");
-                    break;
+    checkUserExist: (req, res) => {
+        proRepo = new ProfileRepository(User);
+        proRepo.findOne({'userName': req.body.userName, 'password': req.body.password})
+        .then(profiles => {
+            if( profiles != null){
+                globalData.setData('myUser', profiles);
+                res.send(profiles);
+            }else{
+                res.send("Invalid Data");
             }
-
-            res.status(200);
-            res.sendFile(Path.join(process.cwd() + "/Front" + pathName), (err) => {
-                if (err) errorHandler(req, res, err);
-            });
-        } catch (err) {
-            return errorHandler(req, res, err);
-        }
-    }
+        })
+        .catch(error => {
+            console.log("Error : ",error);
+        });
+    },
 };
