0220fd4a01de6a353ba0a60f4cb01a2c7aead301
shir149 <80071458+shir149@users.noreply.github.com>
2024-03-07 04:57:51 +0200

    Add files via upload

---
+++ models/profile.model.js
@@ -0,0 +1,21 @@
+const { ObjectId } = require("mongodb");
+const { Schema, model } = require("mongoose");
+const profileSchema = new Schema(
+    {
+        userId: { type: ObjectId, index: 1 },
+        userName: { type: String },
+        firstName: { type: String },
+        lastName: { type: String }, 
+        email: { type: String },
+        password: { type: String },
+        status: { type: Boolean },
+        type: {type: String, enum: ['user', 'admin'] },
+        createdAt: { type: Date, default: Date.now }, 
+        cancelCount: { type: Number, default: 0 } 
+    },
+    {
+        collection: "Profiles", 
+    }
+);
+const profileModel = model("Profiles", profileSchema);
+module.exports = profileModel;
