cc7036e20635a8ce661188f8b16f1341ca44c2a0
Dean Itzhak <Deanitzhak@gmail.com>
2024-03-31 00:52:34 +0300

    Update logInRouter.test.js

--- test/router.test/logInRouter.test.js
+++ test/router.test/logInRouter.test.js
@@ -6,7 +6,6 @@ const loginController = require("../../controllers/logInControler"); // Correcte
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-// Create a mock Express app
 const app = express();
 const loginRouter = express.Router();
 
@@ -16,7 +15,6 @@ app.use('/login', loginRouter);
 
 describe('Login Router', () => {
   it('should check user existence when POST /logIn is called', (done) => {
-    // Assuming you have a valid request body to check user existence
     const userCredentials = {
         userName: 'testuser',
       password: 'testpassword'
@@ -27,10 +25,8 @@ describe('Login Router', () => {
       .send(userCredentials)
       .end((err, res) => {
         expect(res).to.have.status(200);
-        // Add more assertions based on expected response data
         done();
       });
   });
 
-  // Add more test cases for other scenarios, such as invalid credentials, error handling, etc.
 });
